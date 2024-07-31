b62751f26a499c1539dae3d2fe5d68c2a845310a
grs-se <george_ricesmith@hotmail.com>
2024-07-31 17:12:48 +0100

    Push and Pop

--- src/main/java/datastructures/stack/Main.java
+++ src/main/java/datastructures/stack/Main.java
@@ -3,10 +3,13 @@ package datastructures.stack;
 public class Main {
     public static void main(String[] args) {
 
-        Stack myStack = new Stack(4);
+        Stack myStack = new Stack(7);
 
-        myStack.getTop();
-        myStack.getHeight();
+        myStack.push(23);
+        myStack.push(3);
+        myStack.push(11);
+
+        myStack.pop();
 
         myStack.printStack();
     }
