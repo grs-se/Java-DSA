63aef94f3c372dc10b3e19085c735e301f2c4ad8
George Rice-Smith <ricesmg@intgtos.co.uk>
2024-09-10 14:07:39 +0100

    BST: Insert

--- src/main/java/datastructures/trees/Main.java
+++ src/main/java/datastructures/trees/Main.java
@@ -5,6 +5,15 @@ public class Main {
 
         BinarySearchTree myBST = new BinarySearchTree();
 
-        System.out.println("Root = " + myBST.root);
+//        System.out.println("Root = " + myBST.root);
+
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(27);
+
+        System.out.println(myBST.root.left.right.value);
     }
 }
